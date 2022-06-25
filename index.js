a807979b3678a0969eca34e6ab1d511513978c0b
ihuomagold <ihuomagold53@gmail.com>
2022-06-25 14:27:18 +0100

    added html amd js files

---
+++ index.js
@@ -0,0 +1,5 @@
+var firstName = 'Ihuoma';
+var lastName = 'Ogbuji';
+var height = '5.7ft';
+var country = 'Nigeria';
+alert(`My name is ${firstName} ${lastName} I am ${height} tall. My country is ${country}.`)
